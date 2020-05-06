2243b38b3d7f872126bce7ec6a6472a0bdf30f27
MustafaUysal160207077 <mustafa_5397@hotmail.com>
2020-05-06 17:15:28 +0300

    Update view.py

--- view.py
+++ view.py
@@ -1,21 +1,28 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'qt_ilk.ui'
+#
+# Created by: PyQt5 UI code generator 5.6
+#
+# WARNING! All changes made in this file will be lost!
 from PyQt5.uic import loadUi
-from PyQt5 import QtGui
+from PyQt5 import QtGui, QtWidgets
 from PyQt5.QtCore import pyqtSlot
 from PyQt5.QtCore import QTimer
-from PyQt5.QtWidgets import QMainWindow
+from PyQt5.QtWidgets import QMainWindow,QWidget
 from PyQt5.QtGui import QPixmap, QImage
-import cv2
 
 
-class loadUi_example(QMainWindow):
+class loadUi_example(QWidget):
 
     def __init__(self, cam_num):
         super().__init__()
         loadUi("view.ui", self)
         self.cam_num = cam_num
+        self.cam_num.open()
         self.scrollArea.setWidget(self.textEdit) #size of defects will be shown in scrollArea
         self.scrollArea_2.setWidget(self.textEdit_2)
-
+        #self.label_2.setGeometry(75,10,70,170)
         self.timer = QTimer()
         self.timer.timeout.connect(self.readNextFrame)
         self.timer.start(1000 / 24) #24 fps
@@ -24,14 +31,18 @@ class loadUi_example(QMainWindow):
         self.frame = self.cam_num.read()
 
     @pyqtSlot()
-    def on_buton2_clicked(self):
+    def on_pushButton_clicked(self):
         if self.frame is not None:
             image = QImage(self.frame, self.frame.shape[1], self.frame.shape[0],
                            QImage.Format_RGB888)  # The image is stored using a 24-bit RGB format (8-8-8).
             self.pixmap = QPixmap.fromImage(image)
         self.label.setPixmap(self.pixmap)
 
-        lower_sample,upper_sample = self.cam_num.find_sample()
+    @pyqtSlot()
+    def on_pushButton_2_clicked(self):
+
+        
+        lower_sample,upper_sample = self.cam_num.find_biggest_contour()
 
         lowerPartDefects,the_list = self.cam_num.find_defects(lower_sample)
 
@@ -44,8 +55,7 @@ class loadUi_example(QMainWindow):
         upperPartDefects, the_list = self.cam_num.find_defects(upper_sample)
 
         qImg = self.fit_image(upperPartDefects)
-
-        self.label_3.setPixmap(QtGui.QPixmap.fromImage(qImg))
+        self.label_4.setPixmap(QtGui.QPixmap.fromImage(qImg))
 
         self.list_defects(the_list)
         self.textEdit_2.setText(self.line)
@@ -63,3 +73,10 @@ class loadUi_example(QMainWindow):
         bytesPerLine = ch * w
         qImg = QtGui.QImage(image.data, w, h, bytesPerLine, QtGui.QImage.Format_RGB888)
         return qImg
+
+
+
+
+
+
+
