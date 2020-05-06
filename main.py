70a5db6470c62e1b6ca50320e273fbabe8269609
MustafaUysal160207077 <mustafa_5397@hotmail.com>
2020-05-06 17:15:03 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,12 @@ if __name__ == '__main__':
 
     camera = Camera(0)
     app = QApplication([])
-    start_window = loadUi_example(camera)
+    start_window = loadUi_example(camera) #loadUi_example is inheriting Camera class
     start_window.show()
-    app.exit(app.exec_())
+    app.exit(app.exec_())  # QApplication and this line are for creating infinite loop
+    # In this case, the infinite loop is given by app.exec_(). If you remove that line, you will see that the program runs,
+    # but nothing actually happens. . It is important to note that before defining any windows, you should always define the
+    # application in which they are going to run.
+
+
+
